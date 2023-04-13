0975984d7194f6a997ebef6e9e3a65709075a948
epavlova <epavlova@mmtr.ru>
2023-04-13 15:20:54 +0300

    Fix BaseEntity

--- OMyBlog.Domain/Entities/BaseEntity.cs
+++ OMyBlog.Domain/Entities/BaseEntity.cs
@@ -10,6 +10,7 @@ public class BaseEntity
     public Guid Id { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; }
-    public DateTime? DeletedAt { get; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
 }
